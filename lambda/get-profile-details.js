177e89e37122aeef1ee97a59904e3aaad3d5a9df
Manali Shah <mn749104@dal.ca>
2022-07-15 02:24:26 -0300

    Update get-profile-details.js

--- lambda/get-profile-details.js
+++ lambda/get-profile-details.js
@@ -1,13 +1,19 @@
-const AWS = require('aws-sdk');
-
-var ddb = new AWS.DynamoDB({
-    apiVersion: '2012-08-10'
+var AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+var body = "";
+AWS.config.update({
+    region: "us-east-1"
 });
+
+var dynamodb = new AWS.DynamoDB({
+    apiVersion: "2012-08-10"
+});
+
 
 //reference taken from - https://www.fernandomc.com/posts/eight-examples-of-fetching-data-from-dynamodb-with-node/
 async function getUserDetails(params) {
     return new Promise(function(resolve, reject) {
-        ddb.getItem(params, function(err, data) {
+        dynamodb.getItem(params, function(err, data) {
             if (err) {
                 console.log(err);
                 resolve("");
@@ -18,33 +24,64 @@ async function getUserDetails(params) {
     });
 }
 
-async function getTutorList(params) {
-    return new Promise(function(resolve, reject) {
-        console.log("here")
-        ddb.scan(params, function(err, data) {
-            if (err) {
-                console.log(err);
-            } else {
-                
-                var tutors = "{\"tutors\":[";
-                data.Items.forEach(function(element) {
-                    var firstName = "{\"firstName\":\"" + element.firstName.S + "\",";
-                    var lastName = "\"lastName\":\"" + element.lastName.S + "\",";
-                    var mobileNo = "\"mobileNo\":\"" + element.mobileNo.S + "\",";
-                    var email = "\"email\":\"" + element.id.S + "\",";
-                    var expyears = "\"expyears\":" + element.expyears.N + ",";
-                    var expdesc = "\"expdesc\":\"" + element.expdesc.S + "\",";
-                    var skills = "\"skills\":\"" + element.skills.S + "\"";
-                    var tutor = firstName + lastName + mobileNo + email + expyears + expdesc + skills + "},";
-                    tutors = tutors + tutor;
-                });
-                tutors = tutors.slice(0, -1);
-                console.log("here es"+tutors);
-                resolve(tutors + "]}");
+async function getTutorList(Tutorskills) {
+    console.log(Tutorskills)
+    var tutorsSet = new Set();
+    if (Tutorskills === " ") {
+        var params = {
+            TableName: "tutor-details"
+        };
+        var result = await dynamodb.scan(params).promise();
+        for (let i = 0; i < result.Items.length; i++) {
+            var firstName = "{\"firstName\":\"" + result.Items[i].firstName.S + "\",";
+            var lastName = "\"lastName\":\"" + result.Items[i].lastName.S + "\",";
+            var mobileNo = "\"mobileNo\":\"" + result.Items[i].mobileNo.S + "\",";
+            var email = "\"email\":\"" + result.Items[i].id.S + "\",";
+            var expyears = "\"expyears\":" + result.Items[i].expyears.N + ",";
+            var expdesc = "\"expdesc\":\"" + result.Items[i].expdesc.S + "\",";
+            var skill = "\"skills\":\"" + result.Items[i].skills.S + "\"";
+            var tutor = firstName + lastName + mobileNo + email + expyears + expdesc + skill + "}";
+            console.log(tutor)
+            tutorsSet.add(tutor);
+        }
+
+    } else {
+        const skills = Tutorskills.split(",");
+        console.log("len:" + skills.length);
+
+        for (let j = 0; j < skills.length; j++) {
+            try {
+                var params = {
+                    TableName: "tutor-details",
+                    FilterExpression: "contains(skills,:s)",
+                    ExpressionAttributeValues: {
+                        ":s": {
+                            S: skills[j]
+                        }
+                    },
+                };
+                var result = await dynamodb.scan(params).promise()
+                console.log(skills[j] + result.Items.length);
+                for (let i = 0; i < result.Items.length; i++) {
+                    var firstName = "{\"firstName\":\"" + result.Items[i].firstName.S + "\",";
+                    var lastName = "\"lastName\":\"" + result.Items[i].lastName.S + "\",";
+                    var mobileNo = "\"mobileNo\":\"" + result.Items[i].mobileNo.S + "\",";
+                    var email = "\"email\":\"" + result.Items[i].id.S + "\",";
+                    var expyears = "\"expyears\":" + result.Items[i].expyears.N + ",";
+                    var expdesc = "\"expdesc\":\"" + result.Items[i].expdesc.S + "\",";
+                    var skill = "\"skills\":\"" + result.Items[i].skills.S + "\"";
+                    var tutor = firstName + lastName + mobileNo + email + expyears + expdesc + skill + "}";
+                    console.log(tutor)
+                    tutorsSet.add(tutor);
+                }
+            } catch (error) {
+                console.error(error);
             }
-        })
-    });
+        }
+    }
+    return tutorsSet;
 }
+
 
 exports.handler = async (event, context) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
@@ -144,71 +181,25 @@ exports.handler = async (event, context) => {
         }
         body = JSON.stringify(body);
     } else if (JSON.parse(event.body).skills) {
-        console.log("has skills");
+        var tutorStr = "";
         if (JSON.parse(event.body).skills === " ") {
             console.log("has skills=");
-            const params = {
-                TableName: "tutor-details",
-            };
-            body = await getTutorList(params);
-            console.log("body:"+body);
-        } else {
-        var skillset = JSON.parse(event.body).skills.split(",");
-        var tutorsSet = new Set();
-        for (let i = 0; i < skillset.length; i++) {
-            console.log(skillset[i]);
-            var getparams = {
-                TableName: 'skills',
-                Key: {
-                    'skill': {
-                        S: skillset[i]
-                    }
-                }
-            };
-
-            var tutorList = await getUserDetails(getparams);
-            console.log(tutorList);
-            if (tutorList.Item) {
-                for (let j = 0; j < tutorList.Item.tutors.SS.length; j++) {
-                    console.log(tutorList.Item.tutors.SS[j]);
-                    tutorsSet.add(tutorList.Item.tutors.SS[j]);
-                }
-
+            var tutorList = await getTutorList(" ");
+            for (const tutor of tutorList) {
+                tutorStr = tutorStr + tutor + ",";
             }
-        }
-        if (tutorsSet.size > 0) {
-            console.log("here")
-            //body = "{\"tutors\":"+JSON.stringify(Array.from(tutorsSet))+"}";
-            var tutorDetailsArr = [];
-            for (var ele of tutorsSet) {
-                console.log("ele" + ele);
-                var paramsTutor = {
-                    TableName: 'tutor-details',
-                    Key: {
-                        'id': {
-                            S: ele
-                        }
-                    }
-                };
-                var tutordetails = await getUserDetails(paramsTutor);
-                console.log("tutor" + JSON.stringify(tutordetails));
-                var tutorDetails = {
-                    "firstName": tutordetails.Item.firstName.S,
-                    "lastName": tutordetails.Item.lastName.S,
-                    "mobileNo": tutordetails.Item.mobileNo.S,
-                    "email": tutordetails.Item.id.S,
-                    "expyears": tutordetails.Item.expyears.S,
-                    "expdesc": tutordetails.Item.expdesc.S,
-                    "skills": tutordetails.Item.skills.S
-                };
-                tutorDetailsArr = tutorDetailsArr + JSON.stringify(tutorDetails) + ",";
-            }
-            body = "{\"tutors\":[" + tutorDetailsArr + "]}";
+            body = '{"tutors":[' + tutorStr.slice(0, -1) + ']}';
 
         } else {
-            body = "{\"tutors\":[]}";
+            var tutorStr = "";
+            var skillset = JSON.parse(event.body).skills;
+            console.log(skillset);
+            var tutorList = await getTutorList(skillset);
+            for (const tutor of tutorList) {
+                tutorStr = tutorStr + tutor + ",";
+            }
+            body = '{"tutors":[' + tutorStr.slice(0, -1) + ']}';
         }
-    }
     }
 
     return {
